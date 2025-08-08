ce501d92892cb544a304ef5d7b64836a48ed9b8b
Patrick0815300 <patrick-nigrin@web.degit add .git remote add origin https://github.com/Patrick0815300/Testprojekt.git>
2025-08-08 09:37:46 +0200

    feat(Login): begin with Sign Up and create new User in Supabase

--- src/api/supabase/user.ts
+++ src/api/supabase/user.ts
@@ -1,8 +1,6 @@
 import { supabase } from './client.ts'
 
-export async function addUser(email: string, password: string) {
-    const { data, error } = await supabase.auth.signUp({
-        email: 'example@email.com',
-        password: 'example-password',
-    })
-}
+const { data, error } = await supabase.auth.signUp({
+    email: 'example@email.com',
+    password: 'example-password',
+})
