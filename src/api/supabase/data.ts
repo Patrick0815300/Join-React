5b1582167a0e9b1a9b789b2696a4df967d442b3f
Patrick0815300 <patrick-nigrin@web.degit add .git remote add origin https://github.com/Patrick0815300/Testprojekt.git>
2025-08-09 11:10:43 +0200

    feat(Login): add Database request

--- src/api/supabase/data.ts
+++ src/api/supabase/data.ts
@@ -1,7 +1,7 @@
 import { supabase } from './client.ts';
 
-export async function getInstruments() {
-    const { data, error } = await supabase.from('instruments').select();
+export async function getData(database: string) {
+    const { data, error } = await supabase.from(database).select();
     if (error) throw error;
     return data;
 }
--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,18 +1,19 @@
 import { useState, useEffect } from "react"
 import AuthForm from "./AuthForm"
-import { getInstruments } from "../../api/supabase/data"
+import { getData } from "../../api/supabase/data"
 import './Login.modules.scss'
 import { getSession, getUser, signUp } from "../../api/supabase/user"
 
 const Login = ({ }) => {
     const [instrument, setInstrument] = useState<any[]>([]);
     useEffect(() => {
-        getInstruments()
+        getData('contacts')
             .then(data => setInstrument(data))
             .catch(e => console.error(e))
     }, [])
 
-    //console.log('Instrument: ' + instrument);
+    console.log(instrument);
+
 
 
 
