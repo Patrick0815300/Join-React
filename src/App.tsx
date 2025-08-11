afbd9322a0cc60f8f73c54f7d4ada3bb4622a382
Patrick0815300 <patrick-nigrin@web.degit add .git remote add origin https://github.com/Patrick0815300/Testprojekt.git>
2025-08-11 10:37:25 +0200

    feat(Navbar): add Navbar UX/UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css'
-import Login from './components/Auth/Login'
-import SignUp from './components/Auth/SignUp'
-
+import Login from './components/Auth/Login.tsx'
+import SignUp from './components/Auth/SignUp.tsx'
+import { Navbar } from './components/Shared/Navbar.tsx';
 
 function App() {
 
@@ -14,8 +14,10 @@ function App() {
           <Route path="/" element={<Navigate to="/signup" replace />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/navbar" element={<Navbar />} />
         </Routes>
       </BrowserRouter>
+
     </>
   )
 }
