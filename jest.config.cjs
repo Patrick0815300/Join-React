40015be604fd5bc3002d0d396191c7cd2e10d91b
Patrick0815300 <patrick-nigrin@web.degit add .git remote add origin https://github.com/Patrick0815300/Testprojekt.git>
2025-08-19 15:11:39 +0200

    fix somes jest tsconig.app.json problems

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,11 +1,11 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
-    testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],  // Sternchen-Muster richtig ohne Escape
+    testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],  // Pfad richtig mit <rootDir>
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',  // Einfacher Backslash (doppelt, weil JS-String)
+        '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.app.json' }]
     },
     moduleNameMapper: {
         '\\.(css|less|scss)$': 'identity-obj-proxy'
