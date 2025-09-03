612cf4507a1e19afbdcf575622988f017d01eba5
Patrick0815300 <patrick-nigrin@web.degit add .git remote add origin https://github.com/Patrick0815300/Testprojekt.git>
2025-09-03 13:48:39 +0200

    feat(Board): add TaskCard with Data

--- src/components/UI/TaskCardSmall.tsx
+++ src/components/UI/TaskCardSmall.tsx
@@ -10,22 +10,31 @@ interface TaskCardSmallProps {
 }
 
 export function TaskCardSmall({ category, title, description, subtasks, assignetTo, priority }: TaskCardSmallProps) {
+    const getInitials = (fullName: string) => {
+        return fullName
+            .split(' ')
+            .map(name => name.charAt(0).toUpperCase())
+            .join('');
+    }
+
     return (
-        <>
-            <div className={styles.smallCard}>
-                <span className={styles.category}>{category}</span>
-                <span className={styles.title}>{title}</span>
-                <span className={styles.description}>{description}</span>
-                <div className={styles.subtasks}>
-                    <span className={styles.subtasksBar}></span>
-                    <span className={styles.subtaskCount}></span>
-                </div>
-                <div className={styles.bottom}>
-                    <div className={styles.assignetTo}>
-                        <span>ASS:{assignetTo}</span>
-                    </div>
+        <div className={styles.smallCard}>
+            <span className={styles.category}>{category ? category.join(', ') : 'Keine Kategorie vorhanden'}</span>
+            <span className={styles.title}>{title}</span>
+            <span className={styles.description}>{description}</span>
+            <div className={styles.subtasks}>
+                <span className={styles.subtasksBar}></span>
+                <span className={styles.subtaskCount}>{subtasks ? subtasks.length : null}</span>
+            </div>
+            <div className={styles.bottom}>
+                <div className={styles.assignetTo}>
+                    {assignetTo && assignetTo.map((name, index) => (
+                        <span key={index} className={styles.initials}>
+                            {getInitials(name)}
+                        </span>
+                    ))}
                 </div>
             </div>
-        </>
-    )
+        </div>
+    );
 }
