d332a822613a477cd1fc50a7230b3b5a6248e6c5
Patrick0815300 <patrick-nigrin@web.degit add .git remote add origin https://github.com/Patrick0815300/Testprojekt.git>
2025-09-03 11:25:09 +0200

    feat(Board): get Data from supabase, add Task interface

--- src/components/containers/BoardContainer.tsx
+++ src/components/containers/BoardContainer.tsx
@@ -1,9 +1,38 @@
+import { useEffect } from "react";
+import { getData } from "../../api/supabase/data"
 import { Board } from "../presentation/Board"
 
+interface Task {
+    createdAt: Date;
+    title: string;
+    description: string;
+    due_date: Date;
+    priority: string;
+    assignetTo: string[];
+    category: string[];
+    subtasks: string[];
+    phase: string;
+}
+
 export function BoardContainer() {
+    const getTask = async () => {
+        const data = await getData('tasks');
+        if (data) {
+            const todos = data.filter(todos => todos.phase === 'todo');
+            const done = data.filter(todos => todos.phase === 'done');
+            const inProgress = data.filter(todos => todos.phase === 'inProgress');
+            const awaitFeedback = data.filter(todos => todos.phase === 'awaitFeedback');
+        }
+    }
+
+    useEffect(() => {
+        getTask();
+    }, [])
+
+
     return (
         <>
-
+            <Board />
         </>
     )
 }
