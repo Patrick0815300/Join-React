45eb75e4ec6311c219d3fc57b7c7b7f051501f84
Patrick0815300 <patrick-nigrin@web.degit add .git remote add origin https://github.com/Patrick0815300/Testprojekt.git>
2025-09-08 20:55:28 +0200

    feat(Login/Signup): change som style features and fix

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../components/UI/Button"
-import './LoginPage.modules.scss'
 import { LoginContainer } from "../components/containers/LoginContainer";
+import { Link } from 'react-router-dom';
+import styles from './LoginPage.module.scss'
+
 
 export function LoginPage() {
     const navigate = useNavigate();
@@ -11,15 +13,15 @@ export function LoginPage() {
     return (
         <>
             <section>
-                <img className="logo" src="src/assets/icons/logo.svg" alt="Logo" />
-                <header className="head">
+                <img className={styles.logo} src="src/assets/icons/logo.svg" alt="Logo" />
+                <header className={styles.head}>
                     <span>Not a Join user?</span>
                     <Button onClick={() => navigate('/signup')}>Sign up</Button>
                 </header>
                 <LoginContainer />
                 <footer>
-                    <a href="#">Privacy Policy</a>
-                    <a href="#">Legal notice</a>
+                    <Link to="/privacy">Legal Notice</Link>
+                    <Link to="/legal">Privacy Policy</Link>
                 </footer>
             </section>
 
