fd1eca29714d6e2f05e4e586036cdc99d4d4c364
Patrick0815300 <patrick-nigrin@web.degit add .git remote add origin https://github.com/Patrick0815300/Testprojekt.git>
2025-09-09 15:34:57 +0200

    feat(Navbar): change style modul

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
-import './Navbar.modules.scss'
+import styles from './Navbar.module.scss'
 
 export function Navbar() {
     return (
         <>
             <nav>
                 <img src="src/assets/icons/join_logo_vector.svg" alt="Logo" />
-                <ul className="nav-links">
+                <ul className={styles.navLinks}>
                     <li><Link to="/dashboard"><img src="src/assets/icons/dashboard.svg" alt="Dashboard" /> Summary</Link></li>
                     <li><Link to="/tasks"><img src="src/assets/icons/edit.svg" alt="Board" /> Add Task</Link></li>
                     <li><Link to="/board"><img src="src/assets/icons/dashboard.svg" alt="Board" /> Board</Link></li>
