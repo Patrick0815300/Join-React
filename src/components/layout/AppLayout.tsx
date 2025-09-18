760a98e97c4317ea31c65ea6dc331bddd0fdb5a5
Patrick0815300 <patrick-nigrin@web.degit add .git remote add origin https://github.com/Patrick0815300/Testprojekt.git>
2025-09-18 07:11:18 +0200

    feat(Layout): editing style

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../Shared/Header.tsx";
 import { Navbar } from "../Shared/Navbar.tsx";
-import './AppLayout.modules.scss'
+import styles from './AppLayout.module.scss'
 
 export function AppLayout() {
     return (
         <>
-            <div className="layout-container">
+            <div className={styles.layoutContainer}>
                 <Navbar />
-                <div className="wrapper">
+                <div className={styles.wrapper}>
                     <Header />
                     <main>
                         <Outlet />
--- src/pages/BoardPage.tsx
+++
@@ -1,7 +0,0 @@
-export function BoardPage() {
-    return (
-        <>
-
-        </>
-    )
-}
