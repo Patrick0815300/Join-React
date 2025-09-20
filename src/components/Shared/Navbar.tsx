c843a96fa46d010c0f8d12193d6ef69a90b06351
Patrick0815300 <patrick-nigrin@web.degit add .git remote add origin https://github.com/Patrick0815300/Testprojekt.git>
2025-09-20 12:47:16 +0200

    feat(Header): add responsive navbar

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -5,14 +5,14 @@ export function Navbar() {
     return (
         <>
             <nav>
-                <img src="src/assets/icons/join_logo_vector.svg" alt="Logo" />
+                <img className={styles.logo} src="src/assets/icons/join_logo_vector.svg" alt="Logo" />
                 <ul className={styles.navLinks}>
                     <li><Link to="/dashboard"><img src="src/assets/icons/dashboard.svg" alt="Dashboard" /> Summary</Link></li>
                     <li><Link to="/tasks"><img src="src/assets/icons/edit.svg" alt="Board" /> Add Task</Link></li>
                     <li><Link to="/board"><img src="src/assets/icons/dashboard.svg" alt="Board" /> Board</Link></li>
                     <li><Link to="/contacts"><img src="src/assets/icons/contacts.svg" alt="Contacts" /> Contacts</Link></li>
                 </ul>
-                <ul>
+                <ul className={styles.privacy}>
                     <li><Link to="/privacy">Privacy Policy</Link></li>
                     <li><Link to="/legal">Legal notice</Link></li>
                 </ul>
