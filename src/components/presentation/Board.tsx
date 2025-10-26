f3f10de3118460bae4dd5a2a70d8f56383d7b0cb
Patrick0815300 <patrick-nigrin@web.degit add .git remote add origin https://github.com/Patrick0815300/Testprojekt.git>
2025-10-26 16:53:35 +0100

    feat(Board): begin drag drop

--- src/components/presentation/Board.tsx
+++ src/components/presentation/Board.tsx
@@ -11,6 +11,8 @@ interface TaskProps {
     awaitFeedback: Task[];
 }
 
+
+
 export function Board({ todos, done, inProgress, awaitFeedback }: TaskProps) {
     return (
         <>
@@ -24,67 +26,89 @@ export function Board({ todos, done, inProgress, awaitFeedback }: TaskProps) {
 
             <div className={styles.table}>
                 <div className={styles.column}>
-                    <h2>To do</h2>
-                    {todos.map((task, index) => (
-                        <TaskCardSmall
-                            key={index}
-                            taskId={task.id}
-                            category={task.category}
-                            title={task.title}
-                            description={task.description}
-                            subtasks={task.subtasks}
-                            assigned_to={task.assigned_to}
-                            priority={task.priority}
-                        />
-                    ))}
+                    <div className={styles.phase}>
+                        <h2>To do</h2>
+                        <button className={styles.addTaskBtn}>+</button>
+                    </div>
+
+                    {todos.length === 0 ? (
+                        <span className={styles.noTask}>No Tasks To do</span>
+                    ) : (
+                        todos.map((task, _) => (
+                            <TaskCardSmall
+                                key={task.id}
+                                taskId={task.id}
+                                category={task.category}
+                                title={task.title}
+                                description={task.description}
+                                subtasks={task.subtasks}
+                                assigned_to={task.assigned_to}
+                                priority={task.priority}
+                            />
+                        ))
+                    )}
                 </div>
+
+                <div className={styles.column}>
+                    <h2>in progess</h2>
+                    {inProgress.length === 0 ? (
+                        <span className={styles.noTask}>No Tasks in progress</span>
+                    ) : (
+                        inProgress.map((task, _) => (
+                            <TaskCardSmall
+                                key={task.id}
+                                taskId={task.id}
+                                category={task.category}
+                                title={task.title}
+                                description={task.description}
+                                subtasks={task.subtasks}
+                                assigned_to={task.assigned_to}
+                                priority={task.priority}
+                            />
+                        ))
+                    )}
+                </div>
+
+                <div className={styles.column}>
+                    <h2>Await Feedback</h2>
+                    {awaitFeedback.length === 0 ? (
+                        <span className={styles.noTask}>No Tasks in await Feedback</span>
+                    ) : (
+                        awaitFeedback.map((task, _) => (
+                            <TaskCardSmall
+                                key={task.id}
+                                taskId={task.id}
+                                category={task.category}
+                                title={task.title}
+                                description={task.description}
+                                subtasks={task.subtasks}
+                                assigned_to={task.assigned_to}
+                                priority={task.priority}
+                            />
+                        ))
+                    )}
+                </div>
+
+
 
                 <div className={styles.column}>
                     <h2>Done</h2>
-                    {done.map((task, index) => (
-                        <TaskCardSmall
-                            key={index}
-                            taskId={task.id}
-                            category={task.category}
-                            title={task.title}
-                            description={task.description}
-                            subtasks={task.subtasks}
-                            assigned_to={task.assigned_to}
-                            priority={task.priority}
-                        />
-                    ))}
-                </div>
-
-                <div className={styles.column}>
-                    <h2>in Progress</h2>
-                    {inProgress.map((task, index) => (
-                        <TaskCardSmall
-                            key={index}
-                            taskId={task.id}
-                            category={task.category}
-                            title={task.title}
-                            description={task.description}
-                            subtasks={task.subtasks}
-                            assigned_to={task.assigned_to}
-                            priority={task.priority}
-                        />
-                    ))}
-                </div>
-
-                <div className={styles.column}>
-                    <h2>await Feedback</h2>
-                    {awaitFeedback.map((task, index) => (
-                        <TaskCardSmall
-                            key={index}
-                            taskId={task.id}
-                            category={task.category}
-                            title={task.title}
-                            description={task.description}
-                            subtasks={task.subtasks}
-                            assigned_to={task.assigned_to}
-                            priority={task.priority}
-                        />
-                    ))}
+                    {done.length === 0 ? (
+                        <span className={styles.noTask}>No Tasks Done</span>
+                    ) : (
+                        done.map((task, _) => (
+                            <TaskCardSmall
+                                key={task.id}
+                                taskId={task.id}
+                                category={task.category}
+                                title={task.title}
+                                description={task.description}
+                                subtasks={task.subtasks}
+                                assigned_to={task.assigned_to}
+                                priority={task.priority}
+                            />
+                        ))
+                    )}
                 </div>
             </div>
 
